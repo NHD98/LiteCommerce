a491fe33c54dd894cb58feeaadfeab8a788938c3
Admin <ndreece1998@gmail.com>
2020-06-03 20:50:41 +0700

    select all button implement

---
+++ LiteCommerce.Admin/Codes/Actions.js
@@ -0,0 +1,12 @@
+﻿const selectAll = (event) => {
+    const elements = document.querySelectorAll("input[type='checkbox']");
+    if (event.target.checked) {
+        [...elements].forEach(e => {
+            e.checked = true;
+        });
+    } else {
+        [...elements].forEach(e => {
+            e.checked = false;
+        });
+    }
+}
